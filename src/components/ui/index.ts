f755a4f61599aec6160dc8eded437c1a9f14136d
tom <tomswales@creativity.technology>
2025-05-28 08:57:20 +0200

    Refactored test page

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,8 +4,11 @@ import { Caption } from './Caption';
 import { Container } from './Container';
 import { Section } from './Section';
 import { Button } from './Button';
+import { Input } from './Input';
+import { Label } from './Label';
+import { Card, CardHeader, CardBody, CardFooter } from './Card';
 
-export { Heading, Text, Caption, Container, Section, Button };
+export { Heading, Text, Caption, Container, Section, Button, Input, Label, Card, CardHeader, CardBody, CardFooter };
 
 export type { HeadingProps } from './Heading';
 export type { TextProps } from './Text';
@@ -13,3 +16,6 @@ export type { CaptionProps } from './Caption';
 export type { ContainerProps } from './Container';
 export type { SectionProps } from './Section';
 export type { ButtonProps } from './Button';
+export type { InputProps } from './Input';
+export type { LabelProps } from './Label';
+export type { CardProps, CardHeaderProps, CardBodyProps, CardFooterProps } from './Card';
